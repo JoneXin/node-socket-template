1de3d52bd8b0a0b2eee237d759314b2ff539d2b0
zhang.xin <zhang.xin@hzleaper.com>
2022-10-26 09:12:06 +0800

    feat: add tcp client api

--- TCP-SERVER/client/app.ts
+++ TCP-SERVER/client/app.ts
@@ -1,40 +1,45 @@
 import net, { NetConnectOpts, Socket } from 'net';
+import events from 'events';
 
 // 消息类型
 enum Cmd {
-    HEART = "heart",
-    DATA = "data",
-    END = "end"
+    HEART = 'heart',
+    DATA = 'data',
+    END = 'end',
 }
-// 传递的流 类型
+// 数据流 data 类型
 enum DataStreamType {
-    JSON = "json",
+    JSON = 'json',
     IMG = 'img',
-    HTML = 'html'
+    HTML = 'html',
 }
 
+// 消息体json结构
 interface MsgDataStruct {
-    cmd: Cmd,
-    type: DataStreamType,
-    data: any
+    cmd: Cmd;
+    type: DataStreamType;
+    data: any;
 }
 
+// 启动配置
 const SOCKET_OPTIONS = {
     port: 9009,
-    host: '127.0.0.1'
-}
+    // host: '127.0.0.1',
+};
 
-class TcpClient {
-
+class TcpClient extends events.EventEmitter {
     private client: Socket;
-    private rebootTimer: NodeJS.Timer | string = '';
-    private heartTimer: NodeJS.Timer | string = '';
+    private rebootTimer: any;
+    private heartTimer: any;
     private reboostDelay = 4000;
     private heartDelay = 2000;
     private connOption: NetConnectOpts;
+    private rebootTimes: number = 20;
+    private isKeepReboot: boolean = true;
 
     constructor(opt?: NetConnectOpts) {
-        this.connOption = { ...SOCKET_OPTIONS, ...opt || {} };
+        super();
+        this.connOption = { ...SOCKET_OPTIONS, ...(opt || {}) };
         this.client = net.createConnection(this.connOption, () => {
             console.log('client init success!');
 
@@ -43,49 +48,78 @@ class TcpClient {
             this.client.on('end', this.connEndHander.bind(this));
 
             this.heart();
-        })
+        });
     }
 
-    messageHandler = (msg: string) => {
+    private messageHandler = (msg: string) => {
         try {
             const { cmd, type, data } = JSON.parse(msg);
             // 根据cmd处理业务逻辑
-            console.log(cmd, type, data);
-
+            this.emit('msg', cmd, type, data);
         } catch (_) {
             console.error(_);
+            this.emit('msg_error', _);
         }
     };
 
-    errorHandler = (err: Error) => {
+    // 连接错误处理
+    private errorHandler = (err: Error) => {
         console.error(err);
+        this.emit('conn_error', err);
         // 重新连接
         this.rebootTimer || clearInterval(this.rebootTimer);
         this.heartTimer || clearInterval(this.heartTimer);
-        this.rebootTimer = setInterval(() => {
-            console.debug('发生错误，重启中！')
+        if (this.isKeepReboot) {
+            return this.rebootClient();
+        }
+        if (this.rebootTimes-- > 0) {
+            this.rebootClient();
+            console.debug(`剩余重启次数: ${this.rebootTimes}`);
+        }
+    };
+
+    private rebootClient() {
+        this.rebootTimer = setTimeout(() => {
+            console.debug('发生错误，重启中！');
             new TcpClient(this.connOption);
-        }, this.reboostDelay)
+        }, this.reboostDelay);
+    }
+
+    // 断开连接处理
+    private connEndHander = () => {
+        console.warn('连接断开');
     };
 
-    connEndHander = () => {
-        console.warn("连接断开");
-    };
-
-    heart() {
+    private heart() {
         this.heartTimer = setInterval(() => {
-            this.client.write(this.formartData(Cmd.HEART, DataStreamType.JSON, 1));
-        }, this.heartDelay)
+            console.log(1);
+
+            this.client.write(
+                this.formartData(Cmd.HEART, DataStreamType.JSON, 1)
+            );
+        }, this.heartDelay);
     }
 
     // 格式化信息
-    formartData(cmd: Cmd, dataType: DataStreamType, data: any): string {
+    public formartData(cmd: Cmd, dataType: DataStreamType, data: any): string {
         try {
-            return JSON.stringify({ cmd, dataType, data });
+            return JSON.stringify({ cmd, type: dataType, data });
         } catch (_) {
             return '';
-        };
+        }
+    }
+
+    /**
+     *  发送消息
+     * @param cmd 消息体类型
+     * @param type data类型
+     * @param data 数据
+     * @returns boolean
+     */
+    public send(cmd: Cmd, type: DataStreamType, data: any) {
+        return this.client.write(this.formartData(cmd, type, data));
     }
 }
 
-new TcpClient();
+let clinet = new TcpClient();
+clinet.on('msg', (cmd: Cmd, dataType: DataStreamType, data: any) => {});
--- TCP-SERVER/server/app.js
+++ TCP-SERVER/server/app.js
@@ -1,34 +1,35 @@
-const net = require("net");
+const net = require('net');
 
 const init = () => {
-  console.log("其他初始化操作");
+    console.log('其他初始化操作');
 };
 
-const messageHandler = (msg) => {
-  try {
-    const { cmd, data } = JSON.parse(msg);
-    // 根据cmd处理业务逻辑
-  } catch (_) {
-    console.err(_);
-  }
+const messageHandler = msg => {
+    try {
+        const { cmd, type, data } = JSON.parse(msg);
+        console.log(cmd, type, data);
+        // 根据cmd处理业务逻辑
+    } catch (_) {
+        console.err(_);
+    }
 };
 
-const errorHandler = (err) => {
-  console.error(err);
+const errorHandler = err => {
+    console.error(err);
+    console.warn('连接断开');
 };
 
-const connEndHander = () => {
-  console.warn("链接断开");
-};
+const connEndHander = () => {};
 
 // 服务创建
-const server = net.createServer((socket) => {
-  socket.on("ready", init);
-  socket.on("data", messageHandler);
-  socket.on("error", errorHandler);
-  socket.on("end", connEndHander);
+const server = net.createServer(socket => {
+    console.log(socket);
+    socket.on('ready', init);
+    socket.on('data', messageHandler);
+    socket.on('error', errorHandler);
+    socket.on('end', connEndHander);
 });
 
 server.listen(9009, () => {
-  console.log("server start");
+    console.log('server start');
 });
